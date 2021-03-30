f746d08597a99141dee6f6788848dbb8e9cf730e
VivekShetty <sunnykothuru@gmail.com>
2021-03-30 18:53:31 +0530

    updated profile

--- CentralBank/profiles/forms.py
+++ CentralBank/profiles/forms.py
@@ -1,18 +1,6 @@
 from django import forms
 from . import models
 
-# class BasicDetailsForm (forms.ModelForm):
-#     # class to store all the model form fields from models.py
-#     class Meta:
-#         model = models.BasicDetails
-#         fields = ["name", "sex", "DOB", "annual_income", "email", "mobile", "occupation"]
-
-
-# class PresentLocationForm (forms.ModelForm):
-#     class Meta:
-#         model = models.PresentLocation
-#         fields = ["country", "state", "city", "street", "pincode"]
-
 class MoneyTransferForm (forms.ModelForm):
     class Meta:
         model = models.MoneyTransfer
