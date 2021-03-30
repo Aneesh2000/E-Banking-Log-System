f4a3b7faeba1b8f8a3f5ff30d40cfe5d1419fb5d
Aneesh2000 <aneeshgarray2000@gmail.com>
2021-03-30 12:48:55 +0530

    modified dashboard and some changes

---
+++ CentralBank/profiles/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(BasicDetails)
+admin.site.register(PresentLocation)
+admin.site.register(Status)
+admin.site.register(MoneyTransfer)
--- practice/app2/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Employee(models.Model):
-    name =models.CharField(max_length=20)
-    address =models.CharField(max_length=100)
-    salary =models.IntegerField()
-    status =models.BooleanField()
